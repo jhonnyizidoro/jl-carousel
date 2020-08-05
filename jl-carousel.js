aac0f9fa3839458f5b70b46e7ecdcffe6ed205cd
Jhonny Izidoro Menarim <jhonnymenarim@ecompleto.com.br>
2020-08-05 00:46:35 -0300

    Clone nodes when reach end

--- jl-carousel.js
+++ jl-carousel.js
@@ -1,17 +1,43 @@
 const initCarousel = (selector, options) => {
 
-	const carousel = document.querySelector(selector)
-	const slidesWrapper = document.querySelector(`${selector}__slides`)
-	const slides = document.querySelectorAll(`${selector}__slide`)
-	const controlLeft = document.querySelector(`${selector}__control__left`)
-	const controlRight = document.querySelector(`${selector}__control__right`)
-
+	let currentSlide = 0
+	let slides = getSlides(selector)
+	const slidesWrapper = document.querySelector(`${selector} .carousel__slides`)
+	const controlLeft = document.querySelector(`${selector} .carousel__control__left`)
+	const controlRight = document.querySelector(`${selector} .carousel__control__right`)
 	const { perView } = options
 
-	while (perView > slidesWrapper.children.length) {
-		slides.forEach(slide => {
-			slidesWrapper.appendChild(slide.cloneNode(true))
-		})
-	}
+	slides.forEach(slide => slide.style.width = `${100 / perView}%`)
+	controlLeft.classList.add('carousel__control__disabled')
 
+	while (perView > slidesWrapper.children.length) {
+		cloneInTheEnd(slidesWrapper, slides)
+	}
+	slides = getSlides(selector)
+
+	on('click', controlRight, () => {
+		currentSlide = moveSlides(currentSlide, 'right', slides)
+		controlLeft.classList.remove('carousel__control__disabled')
+		if (currentSlide + perView === slidesWrapper.children.length) {
+			cloneInTheEnd(slidesWrapper, slides)
+			slides = getSlides(selector)
+		}
+	})
+
+	on('click', controlLeft, () => {
+		if (currentSlide === 0) {
+			controlLeft.classList.add('carousel__control__disabled')
+		} else {
+			currentSlide = moveSlides(currentSlide, 'left', slides)
+		}
+	})
 }
+
+const moveSlides = (currentSlide, direction, slides) => {
+	const nextSlide = direction === 'left' ? currentSlide - 1 : currentSlide + 1
+	slides.forEach(slide => slide.style.transform = `translateX(${nextSlide * -100}%)`)
+	return nextSlide
+}
+
+const getSlides = selector => document.querySelectorAll(`${selector} .carousel__slide`)
+const cloneInTheEnd = (wrapper, slides) => slides.forEach(slide => wrapper.appendChild(slide.cloneNode(true)))
